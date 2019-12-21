56144bded44d8fa6994b91a0ca87869c5667a73c
fsoeltoni <fsoeltoni@gmail.com>
2019-12-22 04:18:11 +0700

    Create res kecabangan

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import attrs from "./providers/attrs";
 import data from "./providers/data";
 import jenjang_kepangkatan from "./resources/jenjang_kepangkatan";
 import pangkat from "./resources/pangkat";
+import kecabangan from "./resources/kecabangan";
 
 const App = () => (
   <Admin title={attrs.title} dataProvider={data}>
+    <Resource {...kecabangan.identities} />
     <Resource {...pangkat.identities} />
     <Resource {...jenjang_kepangkatan.identities} />
   </Admin>
