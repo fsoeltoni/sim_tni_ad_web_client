ad0cf8edfba59a4ccf886347eb2b3b45e87417a5
fsoeltoni <fsoeltoni@gmail.com>
2019-12-22 03:46:30 +0700

    Configure react-admin

---
+++ src/providers/attrs.js
@@ -0,0 +1,4 @@
+export default {
+  title: "SIM TNI-AD",
+  server: "http://localhost:3000"
+};
---
+++ src/providers/data.js
@@ -0,0 +1,4 @@
+import jsonServerProvider from "ra-data-json-server";
+import attrs from "./attrs";
+
+export default jsonServerProvider(attrs.server);
